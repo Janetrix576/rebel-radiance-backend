fcf5568717d91fdaeb14885e84fa5547f32303dc
Eugene <eugene.chege@student.moringaschool.com>
2025-07-22 14:33:22 +0300

    checkout

--- orders/views.py
+++ orders/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
 
-# Create your views here.
+@api_view(['POST'])
+def create_order(request):
+    data = request.data
+    print("Order received:", data)
+    return Response({"message": "Order received!"}, status=status.HTTP_201_CREATED)
