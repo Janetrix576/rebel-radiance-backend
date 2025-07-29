827cd8beae2b050155c889a5418171c5f06433ae
Stean <stean.ngugi@student.moringaschool.com>
2025-07-29 23:42:40 +0300

    FIX: Add .gitignore and remove secrets from tracking

--- Rebel-radiance/beautyshop/urls.py
+++ Rebel-radiance/beautyshop/urls.py
@@ -27,7 +27,7 @@ urlpatterns = [
     path('api/cart/', include('cart.urls')),
     path('api/orders/', include('orders.urls')),
     path('api/history/', include('history.urls')),
-    path('api/payments/', include('payments.urls')),
+    path('api/payment/', include('payments.urls')), 
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
