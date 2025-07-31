7a8afa1a6eb5cd989526ba3d890b821bb90d2ff8
Stean <stean.ngugi@student.moringaschool.com>
2025-07-31 03:34:39 +0300

    Updated

--- Rebel-radiance/build.sh
+++ Rebel-radiance/build.sh
@@ -1,1 +1,9 @@
-pip install --upgrade...
+
+set -o errexit
+
+pip install --upgrade pip setuptools wheel
+
+pip install -r requirements.txt
+
+python manage.py collectstatic --no-input
+python manage.py migrate
