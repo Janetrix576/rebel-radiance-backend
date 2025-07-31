4a6b19121f72f21f5ab9ae5c995c4bf328d8875d
Stean <stean.ngugi@student.moringaschool.com>
2025-07-31 20:14:33 +0300

    Update views.py

--- Rebel-radiance/authentication/urls.py
+++ Rebel-radiance/authentication/urls.py
@@ -5,7 +5,6 @@ urlpatterns = [
     path('', views.AuthIndexView.as_view(), name='auth_index'), 
     path('register/', views.RegisterView.as_view(), name='register'),
     path('login/', views.LoginView.as_view(), name='login'),
-    path('logout/', views.LogoutView.as_view(), name='logout'), 
     path('social/', include('social_django.urls', namespace='social')),  
     path('google/', views.GoogleLoginView.as_view(), name='google_login'),
 ]
